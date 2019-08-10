307f19cc5329e6f53a729b7f428119ed278b689e
DziejopisWawel <pawel.kurbiel@wp.pl>
2019-08-10 18:30:08 +0200

    refaktoryzacja p_1_4

--- p_1_4.py
+++ p_1_4.py
@@ -1,6 +1,7 @@
 n = 12
 p_len=n*2-1
 hn=0
+even = 0;
 
 def space_creator(p_len, i , reduce):
     n_len= int((p_len - i*2 +1)/2)
@@ -8,20 +9,11 @@ def space_creator(p_len, i , reduce):
         print(' ', end='')
 
 for i in range(1,n+1):
-    if i % 2 == 1:
-        hn += 1;
-        space_creator(p_len, i , 0)
-        for n in range(1, hn+1):
-            print(n, end=' ')
-        for n in range(1, hn):
-            print(hn-n, end=' ')
-        space_creator(p_len, i , 1)
-        print('')
-    if i % 2 == 0:
-        space_creator(p_len, i , 0)
-        for n in range(1, hn+1 ):
-            print(n, end=' ')
-        for n in range(1, hn+1):
-            print(hn-n+1, end=' ')
-        space_creator(p_len, i , 1)
-        print('')
+    hn += i % 2
+    space_creator(p_len, i , 0)
+    for n in range(1, hn+1):
+        print(n, end=' ')
+    for n in range(1, hn + (1 - i % 2)):
+        print(hn - n + (1 - i % 2), end=' ')
+    space_creator(p_len, i , 1)
+    print('')
